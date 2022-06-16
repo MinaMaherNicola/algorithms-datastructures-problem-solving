acdee33b08aa5d8c8023403738e58f3614614bbc
Mina Nicola <minamahernicola@outlook.com>
2022-06-17 01:51:48 +0200

    solved 1.TwoSums problem on LeetCode

--- c-sharp/GrokkingAlgorithms/GrokkingAlgorithms.Console/Program.cs
+++ c-sharp/GrokkingAlgorithms/GrokkingAlgorithms.Console/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using GrokkingAlgorithms.Console.Chapters.Ch1;
+
+int[] test = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+
+Console.WriteLine(BinarySearch.Search(test, -1));
+Console.WriteLine(BinarySearch.Search(test, 1));
+Console.WriteLine(BinarySearch.Search(test, 2));
+Console.WriteLine(BinarySearch.Search(test, 3));
+Console.WriteLine(BinarySearch.Search(test, 4));
+Console.WriteLine(BinarySearch.Search(test, 5));
+Console.WriteLine(BinarySearch.Search(test, 6));
+Console.WriteLine(BinarySearch.Search(test, 7));
+Console.WriteLine(BinarySearch.Search(test, 8));
+Console.WriteLine(BinarySearch.Search(test, 9));
+Console.WriteLine(BinarySearch.Search(test, 10));
+Console.WriteLine(BinarySearch.Search(test, 11));
+Console.WriteLine(BinarySearch.Search(test, 12));
---
+++ c-sharp/LeetCode/LeetCode.Tests/TwoSumsTests1.cs
@@ -0,0 +1,47 @@
+﻿using LeetCode.Console.Problems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LeetCode.Tests
+{
+    public class TwoSumsTests1
+    {
+        private readonly TwoSums1 twoSums;
+
+        public TwoSumsTests1()
+        {
+            twoSums = new();
+        }
+
+        [Theory]
+        [InlineData(new int[] { 1, 2, 3 }, 4, new int[] { 0, 2 })]
+        [InlineData(new int[] { 1, 2, 3 }, 5, new int[] { 1, 2 })]
+        [InlineData(new int[] { 0, 1, 2, 3, 4, 5, 6 }, 10, new int[] { 4, 6 })]
+        [InlineData(new int[] { 0, 1, 2, 3, 4, 5, 6 }, 15, new int[] { -1, -1 })]
+        public void Test_Cases(int[] nums, int target, int[] answer)
+        {
+            var result = twoSums.TwoSum(nums, target);
+
+            Assert.True(IsTwoArraysEqual(result, answer));
+        }
+
+        private bool IsTwoArraysEqual(int[] arr1, int[] arr2)
+        {
+            if (arr1.Length != arr2.Length)
+                return false;
+
+            for(int i = 0; i < arr1.Length; i++)
+            {
+                if (arr1[i] != arr2[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
