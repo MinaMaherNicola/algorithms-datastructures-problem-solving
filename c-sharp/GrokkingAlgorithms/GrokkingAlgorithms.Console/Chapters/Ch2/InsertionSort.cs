29d41a81c48aba55bf8966d912e078ae01c75bb3
Mina Nicola <minamahernicola@outlook.com>
2022-06-18 16:57:52 +0200

    added InsertionSort

---
+++ c-sharp/GrokkingAlgorithms/GrokkingAlgorithms.Console/Chapters/Ch2/InsertionSort.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrokkingAlgorithms.Console.Chapters.Ch2
+{
+    public static class InsertionSort
+    {
+        public static int[] Sort(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int currentMinimal = i;
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[j] < arr[currentMinimal])
+                    {
+                        currentMinimal = j;
+                    }
+                }
+                if (i == currentMinimal)
+                    continue;
+
+                int holder = arr[currentMinimal];
+                arr[currentMinimal] = arr[i];
+                arr[i] = holder;
+            }
+            return arr;
+        }
+    }
+}
