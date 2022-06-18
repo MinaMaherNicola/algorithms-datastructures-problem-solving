9a4b498da5395da37865d191d0fbd32041a7260e
Mina Nicola <minamahernicola@outlook.com>
2022-06-18 17:18:05 +0200

    Solved MedianofTwoSortedArrays

---
+++ c-sharp/LeetCode/LeetCode.Console/Problems/4.MedianOfTwoSortedArrays/MedianOfTwoSortedArraysProblem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Console.Problems._4.MedianOfTwoSortedArrays
+{
+    public class MedianOfTwoSortedArraysProblem
+    {
+        public double FindMedianSortedArrays(int[] nums1, int[] nums2)
+        {
+            var newArr = new int[nums1.Length + nums2.Length];
+            nums1.CopyTo(newArr, 0);
+            nums2.CopyTo(newArr, nums1.Length);
+
+            Array.Sort(newArr);
+            if (newArr.Length % 2 == 0)
+            {
+                return (newArr[newArr.Length / 2] + newArr[(newArr.Length / 2) - 1]) / 2d;
+            }
+            return newArr[newArr.Length / 2];
+        }
+    }
+}
---
+++ c-sharp/LeetCode/LeetCode.Tests/Tests/4.MedianOfTwoSortedArraysTests/MedianOfTwoSortedArraysTest.cs
@@ -0,0 +1,31 @@
+﻿using LeetCode.Console.Problems._4.MedianOfTwoSortedArrays;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LeetCode.Tests.Tests._4.MedianOfTwoSortedArraysTests
+{
+    public class MedianOfTwoSortedArraysTest
+    {
+        private readonly MedianOfTwoSortedArraysProblem median;
+
+        public MedianOfTwoSortedArraysTest()
+        {
+            median = new();
+        }
+
+        [Theory]
+        [InlineData(new int[] { 1, 2, 5, 8 }, new int[] { 4, 6, 7 }, 5d)]
+        [InlineData(new int[] { 1, 2 }, new int[] { 3, 4 }, 2.5d)]
+        [InlineData(new int[] { 1, 2 }, new int[] { 3 }, 2.0d)]
+        public void Test_Cases(int[] arr1, int[] arr2, double answer)
+        {
+            var result = median.FindMedianSortedArrays(arr1, arr2);
+
+            Assert.Equal(answer, result);
+        }
+    }
+}
