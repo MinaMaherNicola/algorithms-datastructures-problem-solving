4343d507c236be45bc429a685e11761a396522d7
Mina Nicola <minamahernicola@outlook.com>
2022-06-18 23:23:31 +0200

    Solved 217-ContainsDuplicate

---
+++ c-sharp/LeetCode/LeetCode.Console/Problems/217.ContainsDuplicate/ContainsDuplicateProblem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Console.Problems._217.ContainsDuplicate
+{
+    public class ContainsDuplicateProblem
+    {
+        public bool ContainsDuplicate(int[] nums)
+        {
+            Dictionary<int, int> appearance = new();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (appearance.ContainsKey(nums[i]))
+                {
+                    appearance[nums[i]]++;
+                    if (appearance[nums[i]] > 1)
+                        return true;
+                }
+                else
+                {
+                    appearance[nums[i]] = 1;
+                }
+
+            }
+            return false;
+        }
+    }
+}
---
+++ c-sharp/LeetCode/LeetCode.Tests/Tests/217.ContainsDuplicateTests/ContainsDuplicateTests.cs
@@ -0,0 +1,32 @@
+﻿using LeetCode.Console.Problems._217.ContainsDuplicate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LeetCode.Tests.Tests._217.ContainsDuplicateTests
+{
+    public class ContainsDuplicateTests
+    {
+        private readonly ContainsDuplicateProblem containsDuplicate;
+
+        public ContainsDuplicateTests()
+        {
+            containsDuplicate = new();
+        }
+
+        [Theory]
+        [InlineData(new int[] { }, false)]
+        [InlineData(new int[] { 1, 2, 3, 4 }, false)]
+        [InlineData(new int[] { 1, 2, 3, 1 }, true)]
+        [InlineData(new int[] { 1, 2, 3, 1, 1, 1, 1 }, true)]
+        public void Test_Cases(int[] arr, bool answer)
+        {
+            var result = containsDuplicate.ContainsDuplicate(arr);
+
+            Assert.Equal(result, answer);
+        }
+    }
+}
