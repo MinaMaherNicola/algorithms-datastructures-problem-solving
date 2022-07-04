a8c46359a0a4eb31e771bccbb6c4b00a67116edc
Mina Nicola <mina.nicola@evapharma.com>
2022-07-05 01:21:36 +0200

    Ugly solution to LongestPalindromicSubstringProblem

---
+++ c-sharp/LeetCode/LeetCode.Tests/Tests/5.LongestPalindromicSubstringTests/LongestPalindromicSubstringTests.cs
@@ -0,0 +1,28 @@
+﻿using LeetCode.Console.Problems._5.LongestPalindromicSubstring;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LeetCode.Tests.Tests._5.LongestPalindromicSubstringTests
+{
+    public class LongestPalindromicSubstringTests
+    {
+        private readonly LongestPalindromicSubstringProblem longestPalindromicSubstring;
+
+        public LongestPalindromicSubstringTests()
+        {
+            longestPalindromicSubstring = new();
+        }
+
+        [Fact]
+        public void Test()
+        {
+            var result = longestPalindromicSubstring.LongestPalindrome("babad");
+
+            Assert.Equal("bab", result);
+        }
+    }
+}
