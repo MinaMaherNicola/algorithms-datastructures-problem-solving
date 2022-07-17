ef0d08aa783728414c7df802a302a6a91326f8fe
Mina Nicola <mina.nicola@evapharma.com>
2022-07-17 21:35:05 +0200

    Solved ZigZag Problem

---
+++ c-sharp/LeetCode/LeetCode.Console/Problems/6.ZigzagConversion/ZigzagConversionProblem.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Console.Problems._6.ZigzagConversion
+{
+    public class ZigzagConversionProblem
+    {
+        public string Convert(string s, int numRows)
+        {
+            if (s.Length == numRows || numRows == 1)
+            {
+                return s;
+            }
+            int sCounter = 0, rowsCounter = 0;
+            List<string[]> listOfArrays = new();
+            string[] arr = new string[numRows];
+            while(sCounter < s.Length)
+            {
+                arr[rowsCounter] = s[sCounter].ToString();
+                if (rowsCounter < numRows - 1)
+                {
+                    rowsCounter++;
+                    sCounter++;
+                    continue;
+                }
+                listOfArrays.Add(arr.ToArray());
+                Array.Clear(arr);
+                sCounter++;
+                rowsCounter--;
+                while (rowsCounter > 0 && sCounter < s.Length)
+                {
+                    arr[rowsCounter] = s[sCounter].ToString();
+                    listOfArrays.Add(arr.ToArray());
+                    Array.Clear(arr);
+                    sCounter++;
+                    rowsCounter--;
+                }
+            }
+            listOfArrays.Add(arr);
+
+            string output = "";
+
+            for (int i = 0; i < numRows; i++)
+            {
+                listOfArrays.ForEach(array =>
+                {
+                    if (array[i] != null)
+                    {
+                        output += array[i].ToString();
+                    }
+                });
+            }
+            return output;
+        }
+    }
+}
---
+++ c-sharp/LeetCode/LeetCode.Tests/Tests/6.ZigzagConversion/ZigzagConversionProblemTests.cs
@@ -0,0 +1,52 @@
+﻿using LeetCode.Console.Problems._6.ZigzagConversion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LeetCode.Tests.Tests._6.ZigzagConversion
+{
+    public class ZigzagConversionProblemTests
+    {
+        private readonly ZigzagConversionProblem zigzagConversionProblem;
+        public ZigzagConversionProblemTests()
+        {
+            zigzagConversionProblem = new();
+        }
+
+        [Fact]
+        public void Test_Case_One()
+        {
+            var result = zigzagConversionProblem.Convert("PAYPALISHIRING", 3);
+
+            Assert.Equal("PAHNAPLSIIGYIR", result);
+        }
+
+        [Fact]
+        public void Test_Case_Two()
+        {
+            var result = zigzagConversionProblem.Convert("PAYPALISHIRING", 4);
+
+            Assert.Equal("PINALSIGYAHRPI", result);
+        }
+
+
+        [Fact]
+        public void Test_Case_Three()
+        {
+            var result = zigzagConversionProblem.Convert("A", 1);
+
+            Assert.Equal("A", result);
+        }
+
+        [Fact]
+        public void Test_Case_Four()
+        {
+            var result = zigzagConversionProblem.Convert("ABCDE", 4);
+
+            Assert.Equal("ABCED", result);
+        }
+    }
+}
